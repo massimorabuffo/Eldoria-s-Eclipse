ae6b53f75a3056772ce3b960f002697358be6dd2
AlessioNatale <alessionatale12@gmail.com>
2023-11-07 14:15:38 +0100

    nuove-modifiche

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,66 +1,41 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import GoogleIcon from "@mui/icons-material/Google";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 import CottageIcon from "@mui/icons-material/Cottage";
 import MailIcon from "@mui/icons-material/Mail";
 import CallIcon from "@mui/icons-material/Call";
 
-
 export function Footer() {
   return (
     <footer className="background-footer">
       <div className="dimensioni-footer">
-
-        <div>
-        <div className="container-footer">
-            <h3>Note finali</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt cupiditate soluta suscipit similique tenetur ab id ea, unde facilis itaque doloribus voluptate maiores, necessitatibus velit? Magni consequuntur ratione labore esse?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem assumenda exercitationem pariatur saepe natus atque magni quaerat. Recusandae mollitia at consequuntur nisi enim iure nostrum quos sit velit rem.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo autem sapiente sit repellat ipsa reprehenderit quos dolorem asperiores aliquam quas, magni dignissimos, consequatur temporibus natus quaerat rem molestias minima voluptatibus.
+        
+          <div className="container-footer">
+            <h3 className="title-footer">Note finali</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              cupiditate soluta suscipit similique tenetur ab id ea, unde
+              facilis itaque doloribus voluptate maiores, necessitatibus velit?
+              Magni consequuntur ratione labore esse? Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Enim rem assumenda exercitationem
+              pariatur saepe natus atque magni quaerat. Recusandae mollitia at
+              consequuntur nisi enim iure nostrum quos sit velit rem.
             </p>
-        </div>
-
-        <div className="button-footer">
-        <Box
-          sx={{
-            "& > :not(style)": {
-              backgroundColor: "white",
-              width: "50ch",
-            },
-          }}
-        >
-          <TextField
-            id="outlined-basic"
-            label="Iscriviti alla nostra newsletter "
-            variant="outlined"
-          />
-        </Box>
-
-        <Button sx={{width:"130px"}} variant="outlined">Invia</Button>
-        </div>
+          </div>
         
-        
-        </div>
 
-       
-        
-       
-
-        
         <div className="separatore">
-
           <div className="contatti-footer">
-            <h3>Quick Links</h3>
+            <h3 className="title-footer">Quick Links</h3>
             <span className="hover-footer">About Us</span>
             <span className="hover-footer">Privacy Policy</span>
             <span className="hover-footer">Contribute</span>
@@ -68,81 +43,122 @@ export function Footer() {
             <span className="hover-footer">Pluto</span>
           </div>
 
-        <div className="contatti-footer">
-          <h3>Tecnologie</h3>
-          <span className="hover-footer">HTML</span>
-          <span className="hover-footer">CSS</span>
-          <span className="hover-footer">Javascript</span>
-          <span className="hover-footer">React</span>
-          <span className="hover-footer">Material UI</span>
-        </div>
+          <div className="contatti-footer">
+            <h3 className="title-footer">Tecnologie</h3>
+            <span className="hover-footer">HTML</span>
+            <span className="hover-footer">CSS</span>
+            <span className="hover-footer">Javascript</span>
+            <span className="hover-footer">React</span>
+            <span className="hover-footer">Material UI</span>
+          </div>
 
-        <div className="contatti-footer">
-          <h3>Contatti</h3>
-          <div className="allineamento-contatti">
-            <CottageIcon sx={{ color: "#adb5bd", "&:hover":{cursor: "pointer", fontSize: 26} }} />
-            <span className="info-contatti">80000,Italia</span>
+          <div className="contatti-footer">
+            <h3 className="title-footer">Contatti</h3>
+            <div className="allineamento-contatti">
+              <CottageIcon
+                sx={{
+                  color: "#adb5bd",
+                  "&:hover": { cursor: "pointer", fontSize: 26 },
+                }}
+              />
+              <span className="info-contatti">80000,Italia</span>
+            </div>
+            <div className="allineamento-contatti">
+              <MailIcon
+                sx={{
+                  color: "#adb5bd",
+                  "&:hover": { cursor: "pointer", fontSize: 26 },
+                }}
+              />
+              <span className="info-contatti">f16team1@gmail.com</span>
+            </div>
+            <div className="allineamento-contatti">
+              <CallIcon
+                sx={{
+                  color: "#adb5bd",
+                  "&:hover": { cursor: "pointer", fontSize: 26 },
+                }}
+              />
+              <span className="info-contatti">081 234 673 86</span>
+            </div>
           </div>
-          <div className="allineamento-contatti">
-            <MailIcon sx={{ color: "#adb5bd", "&:hover":{cursor: "pointer", fontSize: 26} }} />
-            <span className="info-contatti">f16team1@gmail.com</span>
-          </div>
-          <div className="allineamento-contatti">
-            <CallIcon sx={{ color: "#adb5bd", "&:hover":{cursor: "pointer", fontSize: 26} }} />
-            <span className="info-contatti">081 234 673 86</span>
-          </div>
-        
         </div>
-        </div>
-
-       
       </div>
+
+      <div className="newsletter-footer">
+        <h3 className="title-footer">Iscriviti alla nostra newsletter</h3>
+
+        <div className="button-footer">
+          <Box
+            sx={{
+              "& > :not(style)": {
+                backgroundColor: "white",
+                width: "50ch",
+              },
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Inserisci la tua e-mail "
+              variant="outlined"
+            />
+          </Box>
+
+          <Button sx={{ width: "130px" }} variant="outlined">
+            Invia
+          </Button>
+        </div>
+      </div>
+
       <div className="social">
-          <LinkedInIcon
-            sx={{
-              fontSize: 60,
-              color: "#0e76a8",
-              "&:hover": { cursor: "pointer", fontSize: 65},
-            }}
-          />
-          <FacebookIcon
-            sx={{
-              fontSize: 60,
-              color: "#3b5998",
-              "&:hover": { cursor: "pointer", fontSize: 65},
-            }}
-          />
-          <InstagramIcon
-            sx={{
-              fontSize: 60,
-              color: "#E4405F",
-              "&:hover": { cursor: "pointer", fontSize: 65 },
-            }}
-          />
-          <WhatsAppIcon
-            sx={{
-              fontSize: 60,
-              color: "#075E54",
-              "&:hover": { cursor: "pointer", fontSize: 65 },
-            }}
-          />
-          <GoogleIcon
-            sx={{
-              fontSize: 60,
-              color: "#D32F2F",
-              "&:hover": { cursor: "pointer", fontSize: 65},
-            }}
-          />
-          <GitHubIcon 
-              sx={{
-                fontSize: 60,
-                color: "white",
-                "&:hover": { cursor: "pointer", fontSize: 65},
-              }}/>
-        </div>
-        <div className="copyright-footer">
-            <span className="hover-footer">Copyright © 2023 Dungeons & Dragons Reserved by Team1</span>
-        </div>
+        <LinkedInIcon
+          sx={{
+            fontSize: 60,
+            color: "#0e76a8",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+        <FacebookIcon
+          sx={{
+            fontSize: 60,
+            color: "#3b5998",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+        <InstagramIcon
+          sx={{
+            fontSize: 60,
+            color: "#E4405F",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+        <WhatsAppIcon
+          sx={{
+            fontSize: 60,
+            color: "#075E54",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+        <GoogleIcon
+          sx={{
+            fontSize: 60,
+            color: "#D32F2F",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+        <GitHubIcon
+          sx={{
+            fontSize: 60,
+            color: "white",
+            "&:hover": { cursor: "pointer", fontSize: 65 },
+          }}
+        />
+      </div>
+      <div className="copyright-footer">
+        <span className="hover-footer">
+          Copyright © 2023 Dungeons & Dragons Reserved by Team1
+        </span>
+      </div>
     </footer>
   );
 }
--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,71 +1,67 @@
 export function Rules() {
   return (
     <div className="rules">
+      <div className="img-rules"></div>
 
-      <div className="img-rules">
-        
-      </div>
-   
-
-    
       <div className="color-rules">
         <h1>Regole del gioco</h1>
-        <h2>Come giocare a questo fantastico gioco</h2>
+        <h2>Come giocare a questo straordinario gioco</h2>
       </div>
 
-      
-     
-
-     <div className="container-rules-generale">
-     <h2>Regole per i giocatori</h2>
-     <div className="flex-rules">
-      
-        <div className="text-rules">
-        
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            ea, pariatur dignissimos praesentium dolore aut earum quibusdam
-            perspiciatis voluptatum odit illo eos recusandae inventore molestiae
-            iusto cumque consectetur, voluptatem expedita. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Perferendis voluptate praesentium
-            ratione tenetur aperiam eius reiciendis minima iusto voluptatibus!
-            Beatae, quia architecto delectus quaerat voluptates praesentium
-            velit perferendis accusantium aperiam? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Distinctio fugiat at quod explicabo,
-          </p>
+      <div className="container-rules-generale">
+        <h2>Introduzione al gioco</h2>
+        <div className="flex-rules">
+          <div className="text-rules">
+            <p>
+              In Dungeons & Dragons, i giocatori formano un gruppo di
+              avventurieri che esplora mondi fantastici e affronta epiche
+              missioni aumentando il proprio livello di esperienza. In D&D non
+              si vince né si perde... perlomeno non nel modo tradizionale.
+              Dungeons & Dragons è un gioco incentrato sulla narrazione, i dadi
+              ti danno solo una mano. Tutto dipende da te: il tuo aspetto, le
+              tue azioni e come procede la storia. Nella pagina principale
+              inizierai la tua avventura, dovrai scegliere uno tra i personaggi
+              disponibili e ricordati: Non hai scelto un personaggio, tu SEI
+              quel personaggio.
+            </p>
+          </div>
+          <div className="container-img">
+            <img
+              src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/07/Dungeons-and-Dragons-backgrounds.jpg"
+              alt=""
+            />
+          </div>
         </div>
-        <div className="container-img">
-        <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/07/Dungeons-and-Dragons-backgrounds.jpg" alt="" />
+
+        <div className="flex-rules">
+          <div className="container-dragon">
+            <img src="https://wallpapercave.com/dwp1x/wp7069126.jpg" alt="" />
+          </div>
+          <div className="text-rules">
+            <h2>Si entra nell'avventura</h2>
+            <p>
+              I personaggi hanno sei attributi fondamentali: Forza, Destrezza,
+              Costituzione, Intelligenza, Saggezza e Carisma. Questi attributi
+              influenzano il successo delle azioni intraprese dai personaggi.
+              D&D fa largo uso di dadi, in particolare il d20 (dado a 20 facce).
+              I dadi vengono lanciati per determinare il successo in azioni come
+              gli attacchi in combattimento, i tiri salvezza e altre prove. Ogni
+              personaggio ha un numero di punti ferita che rappresenta la sua
+              salute. Se i punti ferita raggiungono zero, il personaggio è
+              ferito o sconfitto. Le azioni sono risolte con i tiri di dado. In
+              combattimento, i personaggi lanciano dadi per determinare se
+              colpiscono e quanto danno infliggono. I personaggi guadagnano
+              esperienza completando missioni e sconfiggendo nemici. Aumentano
+              di livello, acquisiscono nuove abilità e potenziando le
+              caratteristiche. D&D incoraggia la narrazione e il gioco di ruolo,
+              spingendo i giocatori a interpretare i loro personaggi e prendere
+              decisioni basate sulla personalità dei personaggi. I giocatori
+              esplorano mondi fantastici, affrontano mostri, risolvono enigmi e
+              completano missioni guidate dal DM
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="flex-rules">
-      <div className="container-dragon">
-      <img src="https://wallpapercave.com/dwp1x/wp7069126.jpg" alt="" />
-      </div>
-      <div className="text-rules">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis reiciendis rem fuga placeat libero sint quasi aliquid, hic, veritatis cumque, debitis molestiae quisquam maxime nulla consequuntur vitae eveniet voluptatibus totam!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, minus aliquam? Facere iure, aspernatur obcaecati eaque laudantium qui, numquam fugiat quo quos nulla voluptas et adipisci reiciendis nemo, fugit ad!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas delectus magni quam distinctio nisi, amet rerum earum alias, deserunt suscipit, consequuntur ratione? Dolor corrupti, ea repellendus excepturi sequi nesciunt fugiat?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ad rem in accusamus quae at. Eius possimus deserunt doloribus dolor, facilis id, labore eligendi cumque quibusdam sequi mollitia nostrum nihil.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ea sunt magnam ullam quaerat, et quae natus amet ducimus rerum quod odit perspiciatis sapiente aut deleniti quos quasi delectus vero.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptatibus ex perspiciatis quibusdam cumque! Officiis, vitae architecto mollitia ut perferendis dicta provident ea maxime animi obcaecati debitis pariatur amet aliquid?
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi unde error maiores architecto provident laudantium, aliquid voluptatibus vel consectetur repudiandae cum! Minima, atque consectetur cupiditate ducimus possimus nostrum debitis cumque.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quasi dolorem sequi quia excepturi laborum odit facilis, provident laudantium iusto 
-        </p>
-      </div>
-      </div>
-      
-      {/* <img  src="https://images.ctfassets.net/swt2dsco9mfe/4V4u0X8y0ZAuyROgF7jRGI/fd6aa790f745a356a11fbe20abfe19f7/what-is-dnd-1920x600.jpg?q=70&w=1920&h=1342&fit=crop&f=center&fm=webp" alt="" /> */}
-      
-     </div>
-    
-     
-      {/* <img width={"700px"} src="https://images.ctfassets.net/swt2dsco9mfe/Ym9i1haRS4HsElvEa6SqS/604794ba06b9cef049fc141a4dc07513/dnd_home_lorecardv4.png?q=70&w=1342&fm=webp" alt="" />  */}
     </div>
-
-    
-
-    
   );
 }
