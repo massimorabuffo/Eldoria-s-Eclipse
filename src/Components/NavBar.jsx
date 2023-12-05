90c8330474ee89114ecbcbfa8fcd92e9a87221a3
AlessioNatale <alessionatale12@gmail.com>
2023-12-05 13:51:24 +0100

    style navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 import Dice from "./Dice";
 
 function Navbar( {onOpenSideBar, sidebarVisible}) {
